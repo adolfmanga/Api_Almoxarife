8a8e960a3bb59bef485167ab5010a18359baa0ec
adolfmanga <william.rodrigues@pi.estudante.senai.br>
2024-08-06 10:50:19 -0300

    80% em andamento

--- controllers/productscontroller.js
+++ controllers/productscontroller.js
@@ -11,6 +11,68 @@ const getAllproducts = (req, res) => {
  });
 }; 
 
-module.exports = {
-    getAllproducts
+const addproducts = (req, res) => {
+    const { id, nome_do_produto, descricao_do_produto, categoria_do_produto, preco_do_produto, Quantidade_em_estoque, data_de_validade} = req.body;
+    db.query
+    ('INSERT INTO products (id, nome_do_produto, descricao_do_produto, categoria_do_produto, preco_do_produto, Quantidade_em_estoque, data_de_validade) VALUES (?, ?, ?, ?, ?, ?, ?)',
+    
+    [id, nome_do_produto, descricao_do_produto, categoria_do_produto, preco_do_produto, Quantidade_em_estoque, data_de_validade],
+    (err, results) => {
+    if (err) {
+    console.error('Erro ao adicionar produto:', err);
+    res.status(500).send('Erro ao adicionar produto');
+    return;
+    }
+    res.status(201).send('produto adicionada com sucesso');
+    }
+    );};
+   
+// Função para atualizar uma transação existente (substituição completa)
+const updateproductsPut = (req, res) => {
+    const { id } = req.params;
+    const { nome_do_produto, descricao_do_produto, categoria_do_produto, preco_do_produto, Quantidade_em_estoque, data_de_validade} = req.body;
+    db.query(
+    'UPDATE products SET nome_do_produto = ?, descricao_do_produto = ?, categoria_do_produto = ?, preco_do_produto = ?, Quantidade_em_estoque = ?, data_de_validade = ? WHERE id = ?' ,
+    [nome_do_produto, descricao_do_produto, categoria_do_produto, preco_do_produto, Quantidade_em_estoque, data_de_validade, id],
+    (err, results) => {
+    if (err) {
+    console.error('Erro ao atualizar o produto:', err);
+    res.status(500).send('Erro ao atualizar produto');
+    return;
+    }
+    res.send('produto atualizado com sucesso');
+    }
+    );
    };
+// Função para atualizar uma transação existente (atualização parcial)
+const updateproductsPatch = (req, res) => {
+    const { id } = req.params;
+    const fields = req.body;
+    const query = [];
+    const values = [];
+    for (const [key, value] of Object.entries(fields)) {
+    query.push(`${key} = ?`);
+    values.push(value);
+    }
+    values.push(id);
+    db.query(
+    `UPDATE products SET ${query.join(', ')} WHERE id = ?`,
+    values,
+    (err, results) => {
+    if (err) {
+    console.error('Erro ao atualizar produto:', err);
+    res.status(500).send('Erro ao atualizar produto');
+    return;
+    }
+    res.send('produto atualizada com sucesso');
+    }
+    );
+   };
+   module.exports = {
+    getAllproducts,
+    addproducts,
+    updateproductsPut,
+    updateproductsPatch
+   }; 
+   
+   
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,20 @@
 
 const express = require('express'); // Importa o framework Express
+
 const router = express.Router(); // Cria um novo roteador
+
 const productsController = require('../controllers/productsController'); // Importa o controlador de transações
+
 // Definindo uma rota para obter todas as transações
+
 router.get('/', productsController.getAllproducts);
+
+router.post('/', productsController.addproducts); 
+
+// Definindo uma rota para atualizar uma transação existente (substituição completa)
+router.put('/:id', productsController.updateproductsPut);
+// Definindo uma rota para atualizar uma transação existente (atualização parcial)
+router.patch('/:id', productsController.updateproductsPatch);
+
 // Exportando o roteador
 module.exports = router; 
